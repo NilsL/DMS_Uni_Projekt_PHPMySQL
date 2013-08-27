05e6c5ff1f46afe63188dbff97aafbafdb3eb2ef
Nils Lutz <Mail.NLutz@gmail.com>
2013-08-27 15:00:29 +0200

    classification_model.php - changed return variable of create_Classification to return FALSE if  $this->db->insert() fails - clean up get_Classification - added PHPDoc

--- application/models/classification_model.php
+++ application/models/classification_model.php
@@ -1,46 +1,42 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- *
- *
- *
+ * Class Classification_model
  */
 class Classification_model extends CI_Model {
 
    /**
+    * @param $name Name der neuen Klassifizierung
     *
-    *
-    *
+    * @return bool liefert <code> TRUE </code> wenn der Insert erfolgreich war
     */
-	function create_Classification($name) {
-		// mehrfache speicherung ¨¹berpr¨¹fen...
-		$this->db->where ( 'name', $name );
-		$query = $this->db->get ( 'storage_classification' );
-		if ($query->num_rows == 1) {
-			return false;
-		}
-		
-		$data = array (
-				'name' => $name 
-		);
-		$this->db->insert ( 'storage_classification', $data );
-		return true;
-	}
+   function create_Classification($name) {
+      // mehrfache speicherung Ueberpruefen...
+      $this->db->where('name', $name);
+      $query = $this->db->get('storage_classification');
+      if ($query->num_rows == 1) {
+         return FALSE;
+      }
+
+      $data = array('name' => $name);
+      $result = $this->db->insert('storage_classification', $data);
+
+      return $result;
+   }
 
    /**
+    * @param null $id die Ãœbergebene ID der Klassifizierung
     *
-    *
-    *
+    * @return array|bool gibt entweder die gesuchte Klassifizierung zurÃ¼ck oder ein Array fÃ¼r ein Dropdown
     */
-   function get_Classification ($id = NUll) {
-	if (isset ( $id )) {
-		$this->db->where ( 'id', $id );
-		$classification = $this->db->get ( 'storage_classification' );
-		if ($classification->num_rows () > 0) {
-			$row = $classification->result();
-			return $row;
-		}
-	}
+   function get_Classification($id = NULL) {
+      if (isset ($id)) {
+         $this->db->where('id', $id);
+         $classification = $this->db->get('storage_classification');
+         if ($classification->num_rows() > 0) {
+            return $classification->row();
+         }
+      }
       $classifications = $this->db->get('storage_classification');
       if ($classifications->num_rows() > 0) {
          $tmp[] = '--- view all ---';
@@ -55,39 +51,38 @@ class Classification_model extends CI_Model {
       return FALSE;
    }
 
-   
    /**
-    * noch nicht final nur fÃ¼r erste tests. lÃ¤uft aber ohne fehler schon
+    * @param $entered
     *
+    * @return bool
     *
+    * noch nicht final
     */
    function getHints($entered) {
-   	$this->db->like('name', $entered, 'after');
-   	$result = $this->db->get('storage_classification');
-   
-   	if ($result->num_rows() > 0) {
-   		return $result;
-   	}
-   	return FALSE;
+      $this->db->like('name', $entered, 'after');
+      $result = $this->db->get('storage_classification');
+
+      if ($result->num_rows() > 0) {
+         return $result;
+      }
+
+      return FALSE;
    }
-   
+
    /**
-    *
-    *
-    *
+    * @return bool
     */
    function update_Classification() {
-
+      return TRUE;
    }
 
    /**
-    *
-    *
-    *
+    * @return bool
     */
    function delete_Classification() {
-
+      return TRUE;
    }
 }
+
 /* End of file classification_model.php */
 /* Location: ./application/models/classification_model.php */
