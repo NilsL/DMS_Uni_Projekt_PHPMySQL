95e91aee96931bd04ee5cbaabcf30fd088341dac
TaoW <nashmvpnash91@gmail.com>
2013-09-04 18:41:47 +0200

    right side bar mit dem loginsystem verbunden
    Nun hat Guest keinen Zugang zu der detailierten Ansicht zu den Documents

--- application/views/template/right_sidebar.php
+++ application/views/template/right_sidebar.php
@@ -4,10 +4,21 @@
 
       <div class="box">
           <?php
+          $atts = array(
+              'width'      => '500',
+              'height'     => '400',
+              'scrollbars' => 'no',
+              'screenx'    => '500',
+              'screeny'    => '500'
+          );
           $recent_Uploads = $this->sidebar_model->get_recent_Uploads();
           if($recent_Uploads) {
               foreach ($recent_Uploads->result() as $row) {
-                echo anchor_popup('search/popup?doc_id=' . $row->id, '<strong>' . $row->title . '</strong>');
+                if ($this->session->userdata('is_logged_in')) {
+                    echo anchor_popup('search/popup?doc_id=' . $row->id, '<strong>' . $row->title . '</strong>', $atts);
+                } else {
+                    echo '<strong>' . $row->title . '</strong>';
+                }
                 echo br(2);
               }
           }
@@ -23,7 +34,11 @@
         $last_Edited = $this->sidebar_model->get_last_Edited();
         if($last_Edited) {
             foreach ($last_Edited->result() as $row) {
-              echo anchor_popup('search/popup?doc_id=' . $row->id, '<strong>' . $row->title . '</strong>');
+                if ($this->session->userdata('is_logged_in')) {
+                    echo anchor_popup('search/popup?doc_id=' . $row->id, '<strong>' . $row->title . '</strong>', $atts);
+                } else {
+                    echo '<strong>' . $row->title . '</strong>';
+                }
                 echo br(2);
             }
         }
