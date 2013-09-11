8dd8a874ea5130892211587fe788d8247c356a08
Nils Lutz <Mail.NLutz@gmail.com>
2013-09-11 20:45:42 +0200

    changed table headings

--- application/views/mainDirectory_view.php
+++ application/views/mainDirectory_view.php
@@ -3,7 +3,7 @@
 
    <?php
    if (isset ($documents)) {
-      $this->table->set_heading('Title', 'Classification', 'Project', 'Created time', 'Last edited time');
+      $this->table->set_heading('Title', 'Classification', 'Project', 'Created', 'Last edited');
 
       $atts = array(
          'width'      => '500',
@@ -16,12 +16,24 @@
       //sess abfragen zur bestimmung ob popupfenster aufrufbar sein soll
       if ($this->session->userdata('is_logged_in')) {
          foreach ($documents->result() as $row) {
-            $this->table->add_row(anchor_popup('search/popup?doc_id=' . $row->id, '<strong>' . $row->title . '</strong>', $atts), $row->classification, $row->project, mdate("%d-%m-%Y", $row->created), mdate("%d-%m-%Y", $row->last_edited));
+            $this->table->add_row(
+               anchor_popup('search/popup?doc_id=' . $row->id,'<strong>' . $row->title . '</strong>', $atts),
+               $row->classification,
+               $row->project,
+               mdate("%d-%m-%Y", $row->created),
+               mdate("%d-%m-%Y", $row->last_edited)
+            );
          }
       }
       else {
          foreach ($documents->result() as $row) {
-            $this->table->add_row('<strong>' . $row->title . '</strong>', $row->classification, $row->project, mdate("%d-%m-%Y", $row->created), mdate("%d-%m-%Y", $row->last_edited));
+            $this->table->add_row(
+               '<strong>' . $row->title . '</strong>',
+               $row->classification,
+               $row->project,
+               mdate("%d-%m-%Y", $row->created),
+               mdate("%d-%m-%Y", $row->last_edited)
+            );
          }
       }
 
