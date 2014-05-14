7c0361f73fe29a429508debf83d1c7348443c946
Ikarus <Mail.NLutz@gmail.com>
2014-05-14 18:21:13 +0000

    change views to use template

--- application/views/addDokument.php
+++ application/views/addDokument.php
@@ -1,65 +1,6 @@
-<!DOCTYPE html>
-<html lang = "en">
-
-<head>
-    <meta charset = "utf-8">
-    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
-    <meta name = "description" content = "">
-    <meta name = "author" content = "">
-
-    <title>Dokumentenarchiv</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href = "<?= base_url(); ?>css/bootstrap.css" rel = "stylesheet">
-
-    <!-- Add custom CSS here -->
-    <link href = "<?= base_url(); ?>css/bootstrapValidator.css" rel = "stylesheet">
-    <style>
-        body {
-            margin-top: 60px;
-        }
-    </style>
-
-</head>
-
-<body>
-
-<nav class = "navbar navbar-inverse navbar-fixed-top" role = "navigation">
-    <div class = "container">
-        <div class = "navbar-header">
-            <button type = "button" class = "navbar-toggle" data-toggle = "collapse"
-                    data-target = ".navbar-ex1-collapse">
-                <span class = "sr-only">Toggle navigation</span>
-                <span class = "icon-bar"></span>
-                <span class = "icon-bar"></span>
-                <span class = "icon-bar"></span>
-            </button>
-            <a class = "navbar-brand active" href = "<?= site_url(); ?>/">Home</a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class = "collapse navbar-collapse navbar-ex1-collapse">
-            <ul class = "nav navbar-nav">
-                <li><a href = "<?= site_url(); ?>/home/searchDokument">Suchen</a></li>
-                <li class = "dropdown">
-                    <a href = "#" class = "dropdown-toggle" data-toggle = "dropdown">Hinzufügen <b class = "caret"></b></a>
-                    <ul class = "dropdown-menu">
-                        <li><a href = "<?= site_url(); ?>/home/addDokument">Dokument </a></li>
-                        <li><a href = "<?= site_url(); ?>/home/addProjekt">Projekt </a></li>
-                        <li><a href = "<?= site_url(); ?>/home/addAuthor">Author </a></li>
-                    </ul>
-                </li>
-                <li><a href = "http://about.me/nlutz" target = "_blank">About</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><p class="navbar-text">Signed is as <a href = "<?= site_url(); ?>/auth" class="navbar-link"><strong><?= $user_info->username ?></strong></a></p></li>
-                <li><a href = "<?= site_url(); ?>/auth/logout" class="navbar-link"><span class = "glyphicon glyphicon-off"></span> Logout</a></li>
-            </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container -->
-</nav>
+<?php if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+    $this->load->view('header.php'); ?>
 
 <div class = "container">
     <?php if (isset($message)) { ?>
--- application/views/modal.php
+++ application/views/modal.php
@@ -1,3 +1,6 @@
+<?php if (!defined('BASEPATH'))
+    exit('No direct script access allowed'); ?>
+
 <div class = "modal-header">
     <button type = "button" class = "close" data-dismiss = "modal" aria-hidden = "true">&times;</button>
     <h4 class = "modal-title" id = "myModalLabel">Dokument details - <?= $dokument->art; ?></h4>
